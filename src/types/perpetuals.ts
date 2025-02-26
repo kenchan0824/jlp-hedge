bf219e3362900d87d72cfd80bec29282cf2be74e
Kendrick Chan <kenchan@macbook.local>
2025-02-26 16:26:05 +0800

    read jupiter perpetuals custody account

---
+++ src/types/perpetuals.ts
@@ -0,0 +1,38 @@
+import { PublicKey } from "@solana/web3.js";
+import { BN } from "@coral-xyz/anchor";
+
+export type PricingParams = {
+    tradeImpactFeeScalar: number;
+    maxLeverage: number;
+    maxGlobalLongSizes: BN;
+    maxGlobalShortSizes: BN;
+};
+
+export type Assets = {
+    feesReserves: BN;
+    owned: BN;
+    locked: BN;
+    guaranteedUsd: BN;
+    globalShortSizes: BN;
+    globalShortAveragePrices: BN;
+};
+
+export type FundingRateState = {
+    cumulativeInterestRate: BN;
+    lastUpdated: BN;
+    hourlyFundingDbps: number;
+};
+
+export interface CustodyAccount {
+    pool: PublicKey;
+    mint: PublicKey;
+    tokenAccount: PublicKey;
+    decimals: number;
+    isStable: boolean;
+    oracle: PublicKey;
+    pricing: PricingParams;
+    permissions: BN;
+    targetRatioBps: BN;
+    assets: Assets;
+    fundingRateState: FundingRateState;
+} 
